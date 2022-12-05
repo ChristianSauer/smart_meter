7d1dc8f056752bda25dba340916c6ef03f860433
ChristianSauer <christian.sauer@codecentric.de>
2022-12-05 18:55:20 +0100

    error?

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def _delay_for_timeout_seconds():
 
 async def run():
     while True:
+        logger.info("reading data")
         port_settings = data_reader.PortSettings(
             port="/dev/ttyUSB0",
             baudrate=9600,
@@ -37,20 +38,20 @@ async def run():
         # todo catch end but no start errors
         try:
             result = await data_reader.read_one(port_settings)
+
+            val_data = result.data[1].message_body
+            val_data: SmlGetListRes
+            values = val_data.val_list
+            values: typing.List[SmlValListEntry]
+
+            results = {x.obj_name: x.get_scaled_value() for x in values if type(x.value) == int}
+            tags = {x.obj_name: x.unit for x in values if type(x.value) == int}
+
+            _ravendb.store_result(results, tags)
+            _delay_for_timeout_seconds()
         except Exception:
             logger.exception("Unexpected error")
             continue
-
-        val_data = result.data[1].message_body
-        val_data: SmlGetListRes
-        values = val_data.val_list
-        values: typing.List[SmlValListEntry]
-
-        results = {x.get_obis_explanation(): x.get_scaled_value() for x in values if type(x.value) == int}
-        tags = {x.get_obis_explanation(): x.unit for x in values if type(x.value) == int}
-
-        _ravendb.store_result(results, tags)
-        _delay_for_timeout_seconds()
 
 
 if __name__ == '__main__':
